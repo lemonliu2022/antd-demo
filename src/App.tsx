5afaf44ced9a992cc1c1714e84ee84038306a45f
Lemonliu <97353311+lemonliu2022@users.noreply.github.com>
2023-04-27 16:03:51 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from 'antd'
+
+function App() {
+
+  return (
+    <>
+      <Button>{String('你')}{String('好')}</Button>
+    </>
+  )
+}
+
+export default App
